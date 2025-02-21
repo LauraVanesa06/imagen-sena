89a385f4bfc22f7ad03bcb3bd5db4b3697c9a33a
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2025-02-21 13:53:58 -0500

    ...

---
+++ db/migrate/20250219211249_create_libros.rb
@@ -0,0 +1,12 @@
+class CreateLibros < ActiveRecord::Migration[8.0]
+  def change
+    create_table :libros do |t|
+      t.string :autor
+      t.string :nombre
+      t.integer :precio
+      t.integer :cantidad
+
+      t.timestamps
+    end
+  end
+end
